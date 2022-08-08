710b2e1b23ef2167cbf53076fb361009af812325
WannabeRockStar <30549855+WannabeRockStar@users.noreply.github.com>
2022-08-09 00:11:03 +0400

    new data

--- data.js
+++ data.js
@@ -1,51 +1,51 @@
 let data = [
 	{
-		first_name: "Giorgi",
-		last_name: "Gagloshvili",
+		first_name: "Alvaro",
+		last_name: "Aguilar",
 		position: "Front End Developer",
-		salary: 1200,
+		salary: 3000,
 		age: 31
 	},
 	{
-		first_name: "Levan",
-		last_name: "Abramishvili",
+		first_name: "John",
+		last_name: "Smith",
 		position: "Back End Developer",
-		salary: 1500,
+		salary: 3500,
 		age: 28
 	},
 	{
-		first_name: "Nodar",
-		last_name: "Kutchashvili",
+		first_name: "Sarah",
+		last_name: "Winterburn",
 		position: "Hardware Engineer",
-		salary: 1500,
+		salary: 2400,
 		age: 32
 	},
 	{
-		first_name: "Dato",
-		last_name: "Sarjveladze",
+		first_name: "Ilya",
+		last_name: "Vorobiev",
 		position: "Backend Engineer",
-		salary: 1400,
+		salary: 3400,
 		age: 20
 	},
 	{
-		first_name: "Nodar",
-		last_name: "pachulia",
-		position: "Unemployee",
-		salary: 1200,
+		first_name: "Gretta",
+		last_name: "Bohnenkamp",
+		position: "Graphic Designer",
+		salary: 3200,
 		age: 36
 	},
 	{
-		first_name: "Nino",
-		last_name: "Tsintsadze",
-		position: "Immigrant",
-		salary: 1600,
+		first_name: "Ursula",
+		last_name: "Snodgrass",
+		position: "Back End Developer",
+		salary: 2600,
 		age: 29
 	},
 	{
-		first_name: "Jambul",
-		last_name: "Tabatadze",
+		first_name: "Valeri",
+		last_name: "Kuzmin",
 		position: "Engineer",
-		salary: 2100,
+		salary: 3100,
 		age: 59
 	},
 	{
@@ -56,22 +56,22 @@ let data = [
 		age: 25
 	},
 	{
-		first_name: "Sandro",
-		last_name: "Zirakadze",
+		first_name: "Peter",
+		last_name: "Smith",
 		position: "Graphic Designer",
 		salary: 1500,
 		age: 19
 	},
 	{
-		first_name: "Giorgi",
-		last_name: "Tsatsashvili",
+		first_name: "Esteban",
+		last_name: "Vidal",
 		position: "Back-end Developer",
 		salary: 2000,
 		age: 33
 	},
 	{
-		first_name: "Giga",
-		last_name: "Loladze",
+		first_name: "Valentine",
+		last_name: "Gigot",
 		position: "Farmer",
 		salary: 1200,
 		age: 34
